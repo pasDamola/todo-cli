c86203a9b045e500dc8b1ba31a8d67add81057db
pasDamola <oyindadavid18@gmail.com>
2023-12-26 10:47:42 +0100

    adding tests for the to-do API

---
+++ todo_test.go
@@ -0,0 +1,69 @@
+package todo_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/pasDamola/todo-cli"
+)
+
+func TestAdd(t *testing.T) {
+	l := todo.List{}
+
+	taskName := "New Task"
+	l.Add(taskName)
+
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead.", taskName, l[0].Task)
+	}
+}
+
+// TestComplete tests the Complete method of the List type
+func TestComplete(t *testing.T) {
+	l := todo.List{}
+	taskName := "New Task"
+	l.Add(taskName)
+
+	if l[0].Task != taskName {
+		t.Errorf("Expected %q, got %q instead.", taskName, l[0].Task)
+	}
+	if l[0].Done {
+		t.Errorf("New task should not be completed.")
+	}
+
+	l.Complete(1)
+	
+	if !l[0].Done {
+		t.Errorf("New task should be completed.")
+	}
+}
+
+// TestSaveGet tests the Save and Get methods of the List type
+func TestSaveGet(t *testing.T) {
+	l1 := todo.List{}
+	l2 := todo.List{}
+	taskName := "New Task"
+	l1.Add(taskName)
+
+	if l1[0].Task != taskName {
+	  t.Errorf("Expected %q, got %q instead.", taskName, l1[0].Task)
+	}
+	tf, err := os.CreateTemp("", "")
+
+	if err != nil {
+	  t.Fatalf("Error creating temp file: %s", err)
+	}
+
+	defer os.Remove(tf.Name())
+	
+	if err := l1.Save(tf.Name()); err != nil {
+	  t.Fatalf("Error saving list to file: %s", err)
+	}
+	
+	if err := l2.Get(tf.Name()); err != nil {
+	  t.Fatalf("Error getting list from file: %s", err)
+	}
+	if l1[0].Task != l2[0].Task {
+	  t.Errorf("Task %q should match %q task.", l1[0].Task, l2[0].Task)
+	}
+}
