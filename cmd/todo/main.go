86da600c459c080a245345d24a987853389b782c
pasDamola <oyindadavid18@gmail.com>
2023-12-27 11:25:49 +0100

    handling command line with multiple options via the flags pkg

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,9 +1,9 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
-	"strings"
 
 	"github.com/pasDamola/todo-cli"
 )
@@ -14,6 +14,12 @@ const todoFileName = ".todo.json"
 
 func main() {
 
+	// Parsing command line flags
+	task := flag.String("task", "", "Task to be included in the ToDo list")
+	list := flag.Bool("list", false, "List all tasks")
+	complete := flag.Int("complete", 0, "Item to be completed")
+	flag.Parse()
+
 	l := &todo.List{}
 	
 	// Use the Get method to read to do items from file
@@ -22,27 +28,37 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Decide what to do based on the number of arguments provided
+	// Decide what to do based on provided flags
 	switch  {
 		// For no extra arguments, print the list
-	case len(os.Args) == 1:
+	case *list:
 		// List current to do items
 		for _, item := range *l {
 			fmt.Println(item.Task)
 		}
-		
+	
+	case *complete > 0:
+		// Complete the given item
+		if err := l.Complete(*complete); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		} 
 
-	default:
-		// Concatenate all arguments with a spcae
-		item := strings.Join(os.Args[1:], " ")
-
-		l.Add(item)
+	case *task != "":
+		// Add the task
+		l.Add(*task)
 
 		// Save the new list
 		if err := l.Save(todoFileName); err != nil {
 			fmt.Fprintln(os.Stderr, err)
 			os.Exit(1)
-		}
+
+		}		
+
+	default:
+		// Invalid flag provided
+		fmt.Fprintln(os.Stderr, "Invalid option")
+		os.Exit(1)
 	}
 }
 
--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -6,7 +6,6 @@ import (
 	"os/exec"
 	"path/filepath"
 	"runtime"
-	"strings"
 	"testing"
 )
 
@@ -38,27 +37,27 @@ func TestTodoCLI(t *testing.T) {
 	task := "test task number 1"
 	dir, err := os.Getwd()
 	if err != nil {
-	t.Fatal(err)
+		t.Fatal(err)
 	}
 	cmdPath := filepath.Join(dir, binName)
 	
 	t.Run("AddNewTask", func(t *testing.T) {
-	cmd := exec.Command(cmdPath, strings.Split(task, " ")...)
-	if err := cmd.Run(); err != nil {
-	  t.Fatal(err)
-	}
+		cmd := exec.Command(cmdPath, "-task", task)
+		if err := cmd.Run(); err != nil {
+			t.Fatal(err)
+		}
    })
 
    t.Run("ListTasks", func(t *testing.T) {
-	cmd := exec.Command(cmdPath)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-	  t.Fatal(err)
-	}
-	expected := task + "\n"
-	
-	if expected != string(out) {
-	  t.Errorf("Expected %q, got %q instead\n", expected, string(out))
-	}
-  })
+		cmd := exec.Command(cmdPath, "-list")
+		out, err := cmd.CombinedOutput()
+		if err != nil {
+			t.Fatal(err)
+		}
+		expected := task + "\n"
+		
+		if expected != string(out) {
+			t.Errorf("Expected %q, got %q instead\n", expected, string(out))
+		}
+	})
 }
